e4a88969d434ab124af35548f92be7e100f8f94a
simon <30602862+tildeee@users.noreply.github.com>
2021-06-02 00:17:31 -0700

    problem-set-functions: adds echo empty line to custom-snippet test scripts
    Co-authored-by: Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>

--- custom-snippets/giveGreetingChallenge/test.sh
+++ custom-snippets/giveGreetingChallenge/test.sh
@@ -2,6 +2,7 @@
 submission_file=${1:-submission.txt}
 
 cp "$submission_file" submission.js
+echo >> submission.js
 echo "module.exports = {giveCasualGreeting, giveFormalGreeting, writeLetter};" >> submission.js
 
 npm test
--- custom-snippets/isOddNumber/test.sh
+++ custom-snippets/isOddNumber/test.sh
@@ -2,6 +2,7 @@
 submission_file=${1:-submission.txt}
 
 cp "$submission_file" submission.js
+echo >> submission.js
 echo "module.exports = isOddNumber;" >> submission.js
 
 npm test
